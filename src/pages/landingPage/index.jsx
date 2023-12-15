c87837d0d2f2ea63f17c2e25e2878182c6b5ca49
Ismael.Elias <ismael.elias@senior.com.br>
2023-12-14 22:04:21 -0300

    FEAT: Finalizado Landing page

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,48 +1,113 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import LogoSemNome from "../../assets/logo-sem-nome.png";
+import DogLp from "../../assets/dog-lp.png"
+import PetsLp from "../../assets/pets-lp.png"
 
 export default function LandingPage() {
   return (
-    <Container fixed>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          height: "80px",
-        }}
-      >
+    <>
+      <Container fixed>
         <Box
           sx={{
             display: "flex",
-            justifyContent: "center",
+            alignItems: "center",
+            justifyContent: "space-between",
+            height: "80px",
           }}
         >
-          <Link
-            to={"/"}
-            style={{
+          <Box
+            sx={{
               display: "flex",
-              placeItems: "center",
-              textDecoration: "none",
-              color: "black",
-              width: "167px",
-              height: "60px",
+              justifyContent: "center",
             }}
           >
-            <img src={LogoSemNome} alt="logo" width={50} height={50} />
-            <Typography variant="h6" sx={{ flexGrow: 1 }} component="div">
-              Animaverso
+            <Link
+              to={"/"}
+              style={{
+                display: "flex",
+                placeItems: "center",
+                textDecoration: "none",
+                color: "black",
+                width: "167px",
+                height: "60px",
+              }}
+            >
+              <img src={LogoSemNome} alt="logo" width={50} height={50} />
+              <Typography variant="h6" sx={{ flexGrow: 1 }} component="div">
+                Animaverso
+              </Typography>
+            </Link>
+          </Box>
+          <Box sx={{ display: "flex", placeItems: "center", gap: 3 }}>
+            <Link to={"/pet"} style={{ textDecoration: "none", color: "black" }}>
+              Pets
+            </Link>
+            <Button variant="outlined">Entrar</Button>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            marginTop: '100px',
+            width: '100%'
+          }}
+        >
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}>
+            <Box width={'50%'} sx={{
+              paddingLeft: '5%'
+            }}>
+              <Typography fontSize={'2.5rem'}>
+                Uma <b style={{ color: 'var(--primary)' }}>plataforma</b> para você descobrir o universo do <b style={{ color: 'var(--primary)' }}>amor</b>.
+              </Typography>
+              <Box sx={{
+                display: 'flex',
+                alignItems: 'center',
+                height: '42px',
+                mt: '30px'
+              }}>
+                <Button variant="contained">
+                  Adote um amiguinho
+                </Button>
+                <p style={{ margin: '0px 20px' }}>OU</p>
+                <Button variant="outlined">
+                  Cadastre um pet
+                </Button>
+              </Box>
+            </Box>
+            <Box width={'50%'} sx={{
+              display: 'grid',
+              placeItems: 'center'
+            }}>
+              <img src={DogLp} alt="logo" />
+            </Box>
+          </Box>
+          <Box sx={{
+            mt: '10rem',
+            display: 'grid',
+            placeItems: 'center'
+          }}>
+            <Typography fontSize={'2.0rem'} marginBottom={'20px'}>
+              Veja nosso <b style={{ color: 'var(--primary)' }}>universo</b>
             </Typography>
-          </Link>
+            <img src={PetsLp} alt="logo" />
+          </Box>
         </Box>
-        <Box sx={{ display: "flex", placeItems: "center", gap: 3 }}>
-          <Link to={"/pet"} style={{ textDecoration: "none", color: "black" }}>
-            Pets
-          </Link>
-          <Button variant="outlined">Entrar</Button>
-        </Box>
+      </Container>
+      <Box sx={{
+        height: '85px',
+        backgroundColor: '#F6F6F6',
+        display: 'flex',
+        alignItems: 'center',
+        mt: '2rem',
+        paddingLeft: '2rem',
+        borderTop:'1px solid #EEEEEE'
+      }}>
+        <Typography>Direitos reservados ao Animaverso </Typography>
       </Box>
-    </Container>
+    </>
   );
 }
