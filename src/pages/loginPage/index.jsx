894ba467f7f598307416805e5d47a0834c2f8007
Ismael.Elias <ismael.elias@senior.com.br>
2023-12-17 18:33:09 -0300

    FIX: Resolvido problema ao deslogar e acessa a tela de login a pagina redirecionava, agora com a função isEmpty foi validado corretamente para nao redirecionar quando nao deve

--- src/pages/loginPage/index.jsx
+++ src/pages/loginPage/index.jsx
@@ -2,13 +2,14 @@ import { useNavigate } from "react-router-dom";
 import FormLogin from "../../components/formLogin";
 import userUserStore from "../../hooks/userUserStore";
 import { useEffect } from "react";
+import Utils from "../../utils";
 
 export default function LoginPage() {
   const navigate = useNavigate();
   const { user } = userUserStore();
 
   useEffect(() => {
-    if (user) {
+    if (!Utils.isEmpty(user)) {
       navigate("/pet");
       return;
     }
